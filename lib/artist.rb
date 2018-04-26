bb74f697842eb568ccfe91360579d2a9100bb9ef
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-26 14:50:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,6 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1
 
   end
 
@@ -18,12 +17,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+      @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+      @@song_count += 1
 
   end
 
