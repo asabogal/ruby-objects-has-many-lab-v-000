3a7e755d6a688989bec717f3ce644039c0362610
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-26 14:42:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,14 +14,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-
+    binding.pry
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    
+
   end
 
     def self.song_count
